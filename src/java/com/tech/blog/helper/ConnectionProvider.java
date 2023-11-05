7ace34e57c8bc3c0f9e59bea470976bdc26a2349
KISHLAY CHANDAN <75009317+kishlaychandan@users.noreply.github.com>
2023-11-05 14:03:24 +0530

    Update ConnectionProvider.java

--- src/java/com/tech/blog/helper/ConnectionProvider.java
+++ src/java/com/tech/blog/helper/ConnectionProvider.java
@@ -9,7 +9,7 @@ public class ConnectionProvider {
             
             if(con==null){
                 Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/techblog","root","root");
+            con=DriverManager.getConnection("jdbc:mysql://roundhouse.proxy.rlwy.net:36806/railway","root","4fdc5dhhHhCH5CCDh4D32gDAGbGBGAGd");
             
             }   
         } catch (Exception e) {
